05e371ae5fa45df51ab674a864b9c8ac55fe98d0
Marcin Markiewicz <marcin.markiewicz@gemius.com>
2025-02-11 08:45:58 +0100

    fix(keyboard_settings): Change some names, little style fixes

--- mkeybo/components/keyboard_settings.hpp
+++ mkeybo/components/keyboard_settings.hpp
@@ -66,30 +66,27 @@ struct KeyboardSettings
     uint16_t press_min_interval_ms{50};
     uint16_t tap_dance_max_interval_ms{150};
     uint16_t hold_min_interval_ms{200};
-
     uint16_t report_send_interval_ms{5};
 
     uint8_t press_min_interval_cycles{};
     uint8_t tap_dance_max_interval_cycles{};
     uint8_t hold_min_interval_cycles{};
 
-    explicit KeyboardSettings(std::string default_layout_,
-                              const std::vector<KeyboardSettingsLayer<switches_count>*>& layouts_,
-                              const std::vector<KeyboardSettingsLayer<switches_count>*>& layers_,
-                              const std::map<std::string, keyboard_rule_settings::BaseRuleSettings*>& rules_,
-                              const uint16_t switches_refresh_interval_ms_ = 50,
-                              const uint16_t press_min_interval_ms_ = 50,
-                              const uint16_t tap_dance_max_interval_ms_ = 150,
-                              const uint16_t hold_min_interval_ms_ = 200,
-                              const uint16_t report_send_interval_ms_ = 10) :
-        default_layout(std::move(default_layout_)), layouts(layouts_), layers(layers_), rules(rules_),
-        switches_refresh_interval_ms(switches_refresh_interval_ms_), press_min_interval_ms(press_min_interval_ms_),
-        tap_dance_max_interval_ms(tap_dance_max_interval_ms_), hold_min_interval_ms(hold_min_interval_ms_),
-        report_send_interval_ms(report_send_interval_ms_)
+    explicit KeyboardSettings(std::string default_layout,
+                              const std::vector<KeyboardSettingsLayer<switches_count>*>& layouts,
+                              const std::vector<KeyboardSettingsLayer<switches_count>*>& layers,
+                              const std::map<std::string, keyboard_rule_settings::BaseRuleSettings*>& rules,
+                              const uint16_t switches_refresh_interval_ms = 50,
+                              const uint16_t press_min_interval_ms = 50,
+                              const uint16_t tap_dance_max_interval_ms = 150,
+                              const uint16_t hold_min_interval_ms = 200,
+                              const uint16_t report_send_interval_ms = 10) :
+        default_layout(std::move(default_layout)), layouts(layouts), layers(layers), rules(rules),
+        switches_refresh_interval_ms(switches_refresh_interval_ms), press_min_interval_ms(press_min_interval_ms),
+        tap_dance_max_interval_ms(tap_dance_max_interval_ms), hold_min_interval_ms(hold_min_interval_ms),
+        report_send_interval_ms(report_send_interval_ms)
     {
-        press_min_interval_cycles = static_cast<uint8_t>(press_min_interval_ms / switches_refresh_interval_ms);
-        tap_dance_max_interval_cycles = static_cast<uint8_t>(tap_dance_max_interval_ms / switches_refresh_interval_ms);
-        hold_min_interval_cycles = static_cast<uint8_t>(hold_min_interval_ms / switches_refresh_interval_ms);
+        update_computed_settings();
     }
 
     ~KeyboardSettings()
@@ -106,6 +103,14 @@ struct KeyboardSettings
         {
             delete rule;
         }
+    }
+
+protected:
+    void update_computed_settings()
+    {
+        press_min_interval_cycles = static_cast<uint8_t>(press_min_interval_ms / switches_refresh_interval_ms);
+        tap_dance_max_interval_cycles = static_cast<uint8_t>(tap_dance_max_interval_ms / switches_refresh_interval_ms);
+        hold_min_interval_cycles = static_cast<uint8_t>(hold_min_interval_ms / switches_refresh_interval_ms);
     }
 };
 
